010d023e169004f274f1998081a656ab53540d35
Maxim Semenov <maskim.semenov@visyond.com>
2018-06-18 11:45:21 +0300

    Add flask shell context for debug purposes

--- sensors-hive.py
+++ sensors-hive.py
@@ -1,1 +1,7 @@
-from backend import backend 
+from backend import backend, db
+from backend.models import User, Token
+
+
+@backend.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Token': Token}
