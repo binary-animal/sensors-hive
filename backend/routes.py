4463560aa79c431917fb502298c0d1c9ad97b6ad
Maxim Semenov <maskim.semenov@visyond.com>
2018-06-18 11:50:04 +0300

    Now database used for authentication and registration

--- backend/routes.py
+++ backend/routes.py
@@ -1,31 +1,61 @@
-from flask import render_template, flash, redirect, url_for
-from backend import backend
-from backend import common
-from backend.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from backend import backend, common, db
+from backend.forms import LoginForm, RegistrationForm
+from backend.models import User
 
 
 @backend.route('/')
 @backend.route('/index')
+@login_required
 def index():
     sensors = [{'id': 'sensor1', 'Temperature': '39.2', 'Humidity': '44'}, {'id': 'sensor2', 'Temperature': '22', 'Humidity': '21'}]
     res = ""
     for sensor in sensors:
         res += "Sensor: %s<br>Temperature: %s<br>Humidity: %s<br>"%(sensor['id'], sensor['Temperature'], sensor['Humidity'])
 
-    # print(backend.config['SECRET_KEY'])
     return res
     # return render_template('index.html', title='Home', sensors_list=sensors)
 
 
 @backend.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit(): # if GET it False
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-
+    form = LoginForm()
+    if form.validate_on_submit():  # if GET it False
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form)
+
+
+@backend.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@backend.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 
 @backend.route('/api/v1/') # for GET
